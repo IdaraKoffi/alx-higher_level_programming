9d1e8b8a452df34912e3ad3dd0bb8ae9b2f7a4ed
IdaraKoffi <idarakoffi@gmail.com>
2023-11-02 16:54:12 -0700

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
-"""Algorithms for list of integers."""
+""" The technical interview preparation"""
 
 
 def find_peak(list_of_integers):
-    if list_of_integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+    """Finds a peak (i.e. the highest value) of an
+    unsorted list of integers"""
+
+    if list_of_integers == [] or list_of_integers is None:
+        return None
+    # if len(list_of_integers) == 2:
+        # return max(list_of_integers)
+    # if len(list_of_integers) == 3:
+        # if list_of_integers[1] == max(list_of_integers):
+        #   return list_of_integers[1]
+        # return None
+
+    # mid = len(list_of_integers) // 2
+    # left = find_peak(list_of_integers[:mid])
+    # right = find_peak(list_of_integers[mid:])
+    # if left is None:
+        # return right
+    # if right is None:
+        # return left
+    # if left > right:
+        # return left
+    # return right
+
+    list_of_integers.sort()
+    return list_of_integers[-1]
